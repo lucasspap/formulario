1886d5c53c5a6254249992df8fac412827457c92
Lucas Pap <46798017+lucasspap@users.noreply.github.com>
2022-04-21 22:45:50 -0300

    CommitInic
    Formulário utilizando HTML, CSS e JavaScript.

---
+++ js/buscar.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const limparform = (endereco) =>{
+    document.getElementById('endereco').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('cidade').value = '';
+    document.getElementById('estado').value = '';
+}
+
+
+const preencherform = (endereco) =>{
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
+
+const pesquisarCep = async() => {
+    limparform();
+    
+    const cep = document.getElementById('cep').value;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    if (cepValido(cep)){
+        const dados = await fetch(url);
+        const endereco = await dados.json();
+        if (endereco.hasOwnProperty('erro')){
+            document.getElementById('endereco').value = 'O CEP não foi encontrado';
+        }else {
+            preencherform(endereco);
+        }
+    }else{
+        document.getElementById('endereco').value = 'O CEP está incorreto!';
+    }
+     
+}
+
+document.getElementById('cep')
+        .addEventListener('focusout',pesquisarCep);
